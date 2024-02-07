c3ca96ed563d4e8180f73be16c51257fe2e36d9c
Byron Mackay <bqmackay@gmail.com>
2024-02-06 20:33:46 -0700

    updated views

--- polls/views.py
+++ polls/views.py
@@ -1,23 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Question
-from django.template import loader
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("Your looking at quesiton %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", { "question": question })
 
 def results(request, question_id):
-    response = "Your looking at the results of question %s"
-    return HttpResponse(response % question_id)
+    context = { "question_id": question_id}
+    return render(request, "polls/results.html", context)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" % question_id)
+    context = { "question_id": question_id}
+    return render(request, "polls/vote.html", context)
