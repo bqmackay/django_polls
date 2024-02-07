f8f585daccad2cbc68cbbef855dabdeeb4ff3269
Byron Mackay <bqmackay@gmail.com>
2024-02-06 20:44:21 -0700

    updated vercel config

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-6kp)61pziq6luoyt7vjdw2-j3k+@umt_f49bw_kek)z$un%%dr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ ".vercel.app" ]
 
 
 # Application definition
